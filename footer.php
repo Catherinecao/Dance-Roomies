0b33f2e0a58785f991c73f45a405d9254112ea0d
Catherinecao <catherinecao25@gmail.com>
2015-09-09 16:26:36 +1200

    personal page

--- footer.php
+++ footer.php
@@ -1,3 +1,29 @@
+<div class="col-sm-3 bottom-menu clearfix">
+	<div class="col-xs-3 info-item">
+			<a href="#">
+				<i class="fa fa-home fa-lg icon"></i>
+				<span class="badge">2</span>
+			</a>
+		</div>
+		<div class="col-xs-3 info-item">
+			<a href="#">
+				<i class="fa fa-calendar fa-lg icon"></i>
+				<span class="badge">2</span>
+			</a>
+		</div>
+		<div class="col-xs-3 info-item">
+			<a href="#">
+				<i class="fa fa-envelope fa icon"></i>
+				<span class="badge">2</span>
+			</a>
+		</div>
+		<div class="col-xs-3 info-item">
+			<a href="#">
+				<i class="fa fa-ellipsis-v fa-lg icon"></i>
+			</a>
+		</div>
+</div>
+
 <script src="<?php bloginfo('template_url') ?>/js/vendor/jquery-2.1.4.min.js"></script>
 <script src="<?php bloginfo('template_url') ?>/js/vendor/jquery.transit.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
